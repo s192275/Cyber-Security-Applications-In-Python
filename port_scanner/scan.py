e9c0b73e4d79b698e19f5da8767d410868b8a29d
Serhat KILIÇ <96877371+s192275@users.noreply.github.com>
2023-05-04 20:33:08 +0300

    Update scan.py

--- port_scanner/scan.py
+++ port_scanner/scan.py
@@ -16,6 +16,7 @@ for port in range(1,100):
 			print(str(port), "open : Banner : ", httpRcv.decode())
 		else:
 			print(str(port) ,"use different method.")
+	
 	except Exception as e:
 		print(str(port) ,"is closed. Reason : ",str(e))
 		pass
